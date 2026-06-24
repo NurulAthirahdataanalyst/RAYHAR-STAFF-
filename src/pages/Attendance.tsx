d8014662f2c648116a8df0cd9f2facba321b9a62
Nurul Athirah Abdul Rahman <nyn.rahman0236@gmail.com>
2026-06-24 15:11:32 +0800

    fix: unclosed div in Attendance.tsx causing Vercel build error

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -1153,6 +1153,7 @@ export default function Attendance() {
                 )}
               </TableBody>
             </Table>
+            </div>
           </CardContent>
 
           {/* Load More Button */}
