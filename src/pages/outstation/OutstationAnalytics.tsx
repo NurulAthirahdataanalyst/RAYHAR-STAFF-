c8ccc4789fc86c8881a0cf6c47eb0af6a0f30d5d
Nurul Athirah Abdul Rahman <nyn.rahman0236@gmail.com>
2026-07-11 16:07:43 +0800

    Update Outstation Insight

--- src/pages/outstation/OutstationAnalytics.tsx
+++ src/pages/outstation/OutstationAnalytics.tsx
@@ -150,7 +150,7 @@ export default function OutstationAnalytics() {
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-5 gap-4 mb-6">
-          <Card className="lg:col-span-1 min-h-[170px]">
+          <Card className="lg:col-span-1 min-h-[150px]">
             <CardContent className="p-5">
               <div className="flex item-center gap-3">
                 <div className="flex h-12 w-12 items-center justify-center rounded-xl bg-violet-100 text-violet-700 "><Briefcase className="h-6 w-6" /></div>
